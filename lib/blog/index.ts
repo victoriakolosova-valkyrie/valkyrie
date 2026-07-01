7071cc254e555287d474be386b33ab4a3c7cb075
Artis Šteinerts <90416164+ArtisStei@users.noreply.github.com>
2026-07-01 08:20:00 +0300

    Update index.ts
    Sort blog posts by newest date

--- lib/blog/index.ts
+++ lib/blog/index.ts
@@ -1,7 +1,10 @@
-import { blogPosts } from "../blog-posts";
+import { blogPosts as rawBlogPosts } from "../blog-posts";
 
-export { blogPosts };
 export type { BlogPost } from "./types";
+
+export const blogPosts = [...rawBlogPosts].sort(
+  (a, b) => new Date(b.date).getTime() - new Date(a.date).getTime()
+);
 
 export function getBlogPost(slug: string) {
   return blogPosts.find((post) => post.slug === slug);
